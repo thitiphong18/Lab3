6a50675dcb4fafb3a101911db8e283f80ffbdf95
thitiphong18 <118756413+thitiphong18@users.noreply.github.com>
2022-12-06 11:22:05 +0700

    Update 5

--- src/views/PersonView.vue
+++ src/views/PersonView.vue
@@ -2,13 +2,46 @@
 import { reactive, watch } from "vue";
 const person = reactive({ name: "", surname: "", gender: "" });
 const msg = reactive({ name: "", surname: "", gender: "" });
+const checkName = function (name: string) {
+  if (name.trim().length === 0) {
+    msg.name = "First name is empty!!!";
+    return false;
+  }
+  msg.name = "";
+  return true;
+};
+
+const checkSurname = (surname: string) => {
+  if (surname.trim().length === 0) {
+    msg.surname = "Secound name is empty!!!";
+    return false;
+  }
+  msg.surname = "";
+  return true;
+};
+
 watch(
   () => person.name,
   (name) => {
-    if (name.trim().length === 0) {
-      msg.name = "First name is empty!!!";
+    checkName(name);
+  }
+);
+
+watch(
+  () => person.surname,
+  (surname) => {
+    checkSurname(surname);
+  }
+);
+
+watch(
+  () => person.gender,
+  (gender) => {
+    if (gender.trim().length === 0) {
+      msg.gender = "Gender is empty!!!";
+      return;
     }
-    msg.name = "";
+    msg.gender = "";
   }
 );
 
@@ -23,6 +56,7 @@ function doSubmit() {
     <form>
       <label for="name">First Name</label>
       <input type="text" id="name" v-model="person.name" autocomplete="off" />
+      <span class="error">{{ msg.name }}</span>
 
       <label for="surname">Secound Name</label>
       <input
@@ -31,16 +65,18 @@ function doSubmit() {
         v-model="person.surname"
         autocomplete="off"
       />
-      <div>{{ msg.name }}</div>
+      <span class="error">{{ msg.surname }}</span>
+
       <label for="gender">Gender</label>
       <select id="gender" v-model="person.gender">
         <option hidden selected>Select Gender</option>
         <option value="M">Male</option>
         <option value="F">Female</option>
       </select>
+      <span class="error">{{ msg.gender }}</span>
       <input type="submit" value="Submit" @click.prevent="doSubmit" />
     </form>
-    <pre> {{ person }} </pre>
+    <pre> {{ person }} {{ msg }}</pre>
   </div>
 </template>
 
@@ -75,5 +111,10 @@ div {
   border-radius: 5px;
   background-color: #f2f2f2;
   padding: 20px;
+}
+.error {
+  color: red;
+  font-size: smaller;
+  display: block;
 }
 </style>
